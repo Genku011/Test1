c489327c9ea53bda5d8a120b498c5fd6f610ee49
RexTers777 <61738339+RexTers777@users.noreply.github.com>
2021-02-23 11:26:24 +0700

    UpdateFrong2

---
+++ Assets/EiEiInwZa.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EiEiInwZa : MonoBehaviour
+{
+    public float speed;
+    Vector3 startPos;
+
+    void Start()
+    {
+        startPos = transform.position;
+
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.position = (Mathf.Sin(speed/Time.deltaTime)) * startPos;
+    }
+}
